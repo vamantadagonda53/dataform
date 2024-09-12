838a975293bc7ec2647aba1f072d4a45b59e962b
Hemanth Bogavalli <hemanth.bogavalli@spacewell.com>
2024-09-12 05:57:27 +0000

    Latest Changes

---
+++ definitions/env_var.js
@@ -0,0 +1,8 @@
+module.exports = {
+  project_id:"cobundu-bi-playground",
+  chemours_p_dataset_name: "CHEMOURS_P",
+  chemours_t_dataset_name: "CHEMOURS_T",
+  arla_p_dataset_name: "ARLA",
+  nordea_p_dataset_name: "NORDEA_P",
+  nordea_t_dataset_name: "NORDEA_T"
+};
--- definitions/template.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  project_id:"cobundu-bi-playground",
-  dataset_name: "CHEMOURS_P"
-};
