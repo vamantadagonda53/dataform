ed7ce3832e8c2b604dc13d9bc13f17c4fc8fc6e8
Vaman Tadagonda <vaman.tadagonda@spacewell.com>
2025-01-16 18:21:23 +0000

    Comitting new version for mcs_stream_V_22

---
+++ definitions/env_var.js
@@ -0,0 +1,9 @@
+module.exports = {
+  project_id:"cobundu-bi-playground",
+  chemours_p_dataset_name: "CHEMOURS_P",
+  chemours_t_dataset_name: "CHEMOURS_T",
+  arla_p_dataset_name: "ARLA",
+  nordea_p_dataset_name: "NORDEA_P",
+  nordea_t_dataset_name: "NORDEA_T",
+  nordea_p_ver_num: 21
+};
